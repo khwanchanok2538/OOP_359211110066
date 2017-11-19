f5021d1467ba55040b39ead916d8e22d01fe8fa3
khwanchanok2538 <k_love38@hotmail.com>
2017-11-19 09:49:05 +0700

    Testing upload to Giuhub

---
+++ src/oop_lab/s359211110066/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lab.s359211110066.rmutsv.com;
+
+public class HelloWorld
+{
+//    Create main method
+public static void main(String[] args) {
+    System.out.println("Hello world, I'm KHWAN");
+    System.out.println("My name is khwanchanok kana");
+
+
+}//nain
+
+}// class
