715137912dab9283f7e831515c83d822f17c4bc9
khwanchanok2538 <k_love38@hotmail.com>
2017-11-20 13:41:38 +0700

    Add Information

--- src/oop_lab/s359211110066/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110066/rmutsv/com/HelloWorld.java
@@ -7,7 +7,9 @@ public static void main(String[] args) {
     System.out.println("Hello world, I'm KHWAN");
     System.out.println("My name is khwanchanok kana");
 //Add information
-System.out.println("Faculty: Management Technology");
-System.out.println("RMUTSV");
+    System.out.println("Faculty: Management Technology");
+    System.out.println("RMUTSV");
+
+    }//main
 
 }// class
