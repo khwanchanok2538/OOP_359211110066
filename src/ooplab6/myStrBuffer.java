769c613f52964784ea7a43325769f73e11f57eab
khwanchanok2538 <k_love38@hotmail.com>
2017-12-25 18:08:38 +0700

    compareString

---
+++ src/ooplab6/myStrBuffer.java
@@ -0,0 +1,20 @@
+package ooplab6;
+
+public class myStrBuffer {
+    public static void main(String[] args) {
+//        StringBuffer
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Hello");
+        buffer.append(" ");
+        buffer.append("to");
+        buffer.append(" ");
+        buffer.append("RMUTSV");
+        buffer.append(" ");
+        buffer.append(2017);
+        System.out.println(buffer);
+        buffer.replace(0, 4,"University of");
+        System.out.println(buffer);
+
+    }//main
+}//class
