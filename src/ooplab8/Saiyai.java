b01c92c031fbb0e1cfa42a71ff57788a4208f872
khwanchanok2538 <k_love38@hotmail.com>
2018-01-29 15:48:21 +0700

    Campus

--- src/ooplab8/Saiyai.java
+++ src/ooplab8/Saiyai.java
@@ -1,4 +1,14 @@
 package ooplab8;
 
-public class Saiyai {
+public class Saiyai implements Campus {
+    @Override
+    public void getCampusName() {
+        System.out.println("Saiyai Campus.");
+    }
+
+    @Override
+    public void getArea() {
+        System.out.println("Thugsong Nakhonsithammarat.");
+
+    }
 }
